50afd8e1a396cff6840eb80642355653091643db
Kamil <kamil.andrzejewski13@gmail.com>
2016-09-01 17:28:02 +0200

    Second version

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,259 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class Map : MonoBehaviour
+{
+    // public:
+    public void InitializeMap(
+        GameObject wall,
+        Vector3 middle,
+        uint mazeSize)
+    {
+        Initialize(wall, middle, mazeSize);
+
+        Debug.Assert(mazeSize > 0, "mazeSize is out of range");
+
+        Triangles = new List<Triangle>();
+
+        int index = 0;
+        CreateMap(index);
+        SetNeighbourhood();
+
+        CreateMaze();
+
+        Debug.Log("End of creation");
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    public Vector3 Middle
+    {
+        get
+        {
+            return middle;
+        }
+
+        set
+        {
+            middle = value;
+        }
+    }
+
+    public GameObject Wall
+    {
+        get
+        {
+            return wall;
+        }
+
+        set
+        {
+            wall = value;
+        }
+    }
+
+    public List<Triangle> Triangles
+    {
+        get
+        {
+            return triangles;
+        }
+
+        set
+        {
+            triangles = value;
+        }
+    }
+
+    //private:
+    private List<Triangle> triangles;
+    private Dictionary<Vector3, int> trianglesIndices = new Dictionary<Vector3, int>();
+    private int roundingHelper = 1000;
+    private GameObject wall;
+    private Vector3 middle;
+    private float wallWidth;
+    private uint mazeSize;
+
+    private void Initialize(GameObject wall, Vector3 middle, uint size)
+    {
+        wallWidth = wall.transform.lossyScale.z;
+        gameObject.name = "Map";
+        Middle = middle;
+        mazeSize = size;
+        Wall = wall;
+    }
+
+    private void CreateMap(
+        int index)
+    {
+        Queue<Triangle> triangleQueue = new Queue<Triangle>();
+
+        CreateFirstLevelOfTriangles(triangleQueue, ref index);
+
+        //common
+        Triangle currentTriangle;
+        int yRotation;
+        int neighbourYRotation;
+
+        //single triangle
+        Triangle.Drawing shouldBeDrawn;
+        Vector3 neighbourPosition;
+
+        while (triangleQueue.Count > 0)
+        {
+            currentTriangle = triangleQueue.Dequeue();
+            Debug.Assert(currentTriangle, "Triangle doesn't exist");
+            yRotation = (int)Mathf.Round(currentTriangle.Rotation.y);
+            neighbourYRotation = (yRotation + 180) % 360;
+            shouldBeDrawn = currentTriangle.ShouldBeDrawn == Triangle.Drawing.DrawAll ? Triangle.Drawing.NoDraw : Triangle.Drawing.DrawAll;
+
+            if (yRotation == (int)Mathf.Round(currentTriangle.Sector * 60.0f))
+            {
+                if (currentTriangle.Level >= mazeSize)
+                {
+                    continue;
+                }
+
+                neighbourPosition = currentTriangle.GetNeighbourPosition(Triangle.Direction.TriangleBase);
+                MakeTriangle(triangleQueue, neighbourPosition, neighbourYRotation, currentTriangle.Level + 1, currentTriangle.Sector, shouldBeDrawn, ref index);
+            }
+            else
+            {
+                if (currentTriangle.Level == mazeSize)
+                {
+                    shouldBeDrawn = yRotation % 120 == 0 ? Triangle.Drawing.DrawBase : Triangle.Drawing.DrawAll;
+                }
+
+                neighbourPosition = currentTriangle.GetNeighbourPosition(Triangle.Direction.LeftLeg);
+                MakeTriangle(triangleQueue, neighbourPosition, neighbourYRotation, currentTriangle.Level, currentTriangle.Sector, shouldBeDrawn, ref index);
+
+                neighbourPosition = currentTriangle.GetNeighbourPosition(Triangle.Direction.RightLeg);
+                MakeTriangle(triangleQueue, neighbourPosition, neighbourYRotation, currentTriangle.Level, currentTriangle.Sector, shouldBeDrawn, ref index);
+            }
+        }
+    }
+
+    private void CreateFirstLevelOfTriangles(
+        Queue<Triangle> triangleQueue,
+        ref int index)
+    {
+        float x, z;
+        Vector3 triangleMiddle;
+        int level = 1;
+        int sector;
+        Triangle.Drawing shouldBeDrawn;
+
+        for (int i = 0; i < 6; i++)
+        {
+            x = Common.GetAxisCoordinate(wallWidth, i * 60, Mathf.Sin);
+            z = Common.GetAxisCoordinate(wallWidth, i * 60, Mathf.Cos);
+            triangleMiddle = new Vector3(x + Middle.x, 0.0f, z + Middle.z);
+
+            sector = i;
+            shouldBeDrawn = (i % 2) == 0 ? Triangle.Drawing.DrawAll : Triangle.Drawing.NoDraw;
+
+            MakeTriangle(triangleQueue, triangleMiddle, i * 60.0f, level, sector, shouldBeDrawn, ref index);
+        }
+    }
+
+    private void MakeTriangle(
+        Queue<Triangle> triangleQueue,
+        Vector3 triangleMiddle,
+        float yRotation,
+        int level,
+        int sector,
+        Triangle.Drawing shouldBeDrawn,
+        ref int index)
+    {
+        Vector3 roundedMiddle = new Vector3(Mathf.Round(triangleMiddle.x * roundingHelper), 0.0f, Mathf.Round(triangleMiddle.z * roundingHelper));
+
+        if (!trianglesIndices.ContainsKey(roundedMiddle))
+        {
+            Vector3 triangleRotation = new Vector3(0.0f, yRotation, 0.0f);
+            GameObject newGameObject = new GameObject();
+            Triangle newTriangle = newGameObject.AddComponent<Triangle>() as Triangle;
+            newTriangle.InitializeTriangle(Wall, triangleMiddle, triangleRotation, level, sector, shouldBeDrawn);
+            newTriangle.transform.parent = gameObject.transform;
+            Triangles.Add(newTriangle);
+            trianglesIndices.Add(roundedMiddle, index);
+            triangleQueue.Enqueue(Triangles[index++]);
+        }
+        else
+        {
+            Debug.Log("Triangle already exists");
+        }
+    }
+
+    private int GetTriangleIndex(
+        Vector3 position)
+    {
+        Vector3 roundedMiddle = new Vector3(Mathf.Round(position.x * roundingHelper), 0.0f, Mathf.Round(position.z * roundingHelper));
+        return trianglesIndices.ContainsKey(roundedMiddle) ? trianglesIndices[roundedMiddle] : - 1;
+    }
+
+    private void SetNeighbourhood()
+    {
+        Vector3 neighbourPosition;
+        int neighbourIndex;
+        Debug.Assert(Triangles[0], "Triangles don't exist.");
+        int numberOfNeighbours = Triangles[0].Neighbours.Length;
+        //GameObject neighbour; // TODO: delete
+        //int j = 0; // TODO: delete
+
+        foreach (Triangle currentTriangle in Triangles)
+        {
+            for (int i = 0; i < numberOfNeighbours; i++)
+            {
+                neighbourPosition = currentTriangle.GetNeighbourPosition((Triangle.Direction)i);
+                neighbourIndex = GetTriangleIndex(neighbourPosition);
+                if(neighbourIndex >= 0)
+                {
+                    //neighbour = new GameObject(); // TODO: delete
+                    //neighbour.transform.position = neighbourPosition; // TODO: delete
+                    //neighbour.name = "Neighbour" + j; // TODO: delete
+                    currentTriangle.Neighbours[i] = Triangles[neighbourIndex];
+                }
+            }
+
+            if (currentTriangle.ShouldBeDrawn != Triangle.Drawing.DrawAll)
+            {
+                currentTriangle.SetRefToWalls();
+            }
+            //j++; // TODO: delete
+        }
+    }
+
+    private void CreateMaze()
+    {
+        System.Random rnd = new System.Random();
+        Stack<Triangle> lastCells = new Stack<Triangle>();
+
+        int randomIndex = UnityEngine.Random.Range(0, Triangles.Count);
+        Triangle currentCell = Triangles[randomIndex];
+        Triangle neighbour;
+        currentCell.Visited = true;
+
+        do
+        {
+            neighbour = currentCell.GetRandomNeighbourAndDeleteWall();
+            if (neighbour)
+            {
+                lastCells.Push(currentCell);
+                currentCell = neighbour;
+                currentCell.Visited = true;
+            }
+            else
+            {
+                currentCell.IsDeadEnd = true;
+                currentCell = lastCells.Pop();
+            }
+        } while (lastCells.Count > 0);
+    }
+
+
+}
---
+++ Assets/Scripts/Triangle.cs
@@ -0,0 +1,356 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class Triangle : MonoBehaviour
+{
+    /*
+        * Triangle:
+        * Middle = { 0.0 }, Rotation = { 0.0 }
+        * 
+        * Left:
+        *  Pos:
+        *      x = -a/4
+        *      y = 0.0
+        *      z = a*sqrt(3)/4 - a*sqrt(3)/6
+        *  Rot:
+        *      x = 0.0
+        *      y = 30.0
+        *      z = 0.0    
+        * Rigth:
+        *  Pos:
+        *      x = a/4
+        *      y = 0.0
+        *      z = a*sqrt(3)/4 - a*sqrt(3)/6
+        *  Rot:
+        *      x = 0.0
+        *      y = 150.0
+        *      z = 0.0
+        * Base:
+        *  Pos:
+        *      x = 0.0
+        *      y = 0.0
+        *      z = -a*sqrt(3)/6
+        *  Rot:
+        *      x = 0.0
+        *      y = 0.0
+        *      z = 0.0
+        */
+
+    //public:
+    public enum Drawing
+    {
+        NoDraw,
+        DrawBase,
+        DrawAll
+    }
+
+    public enum Direction
+    {
+        TriangleBase,
+        LeftLeg,
+        RightLeg
+    }
+
+    public void InitializeTriangle(
+        GameObject wall,
+        Vector3 middle,
+        Vector3 rotation,
+        int level,
+        int sector,
+        Drawing shouldBeDrawn)
+    {
+        Initialize(wall, middle, rotation, level, sector, shouldBeDrawn);
+
+        if (ShouldBeDrawn != Drawing.NoDraw)
+        {
+            if (ShouldBeDrawn == Drawing.DrawAll)
+            {
+                Walls[1] = GetLeftLeg(wall, middle, rotation);
+                Walls[2] = GetRightLeg(wall, middle, rotation);
+            }
+            Walls[0] = GetTriangleBase(wall, middle, rotation);
+
+            HoldWalls();
+        }
+
+        SetTriangleOnRightPosition();
+    }
+
+    public void SetRefToWalls()
+    {
+        Debug.Assert(ShouldBeDrawn != Drawing.DrawAll, "Wrong usage of SetRefToWalls");
+        if(Neighbours[(int)Direction.LeftLeg] != null)
+        {
+            Debug.Assert(Neighbours[(int)Direction.LeftLeg].Walls[(int)Direction.RightLeg], "Wrong usage of SetRefToWalls");
+            if(Sector == Neighbours[(int)Direction.LeftLeg].Sector)
+            {
+                Walls[(int)Direction.LeftLeg] = Neighbours[(int)Direction.LeftLeg].Walls[(int)Direction.LeftLeg];
+            }
+            else
+            {
+                Walls[(int)Direction.LeftLeg] = Neighbours[(int)Direction.LeftLeg].Walls[(int)Direction.RightLeg];
+            }
+        }
+        if (Neighbours[(int)Direction.RightLeg] != null)
+        {
+            Debug.Assert(Neighbours[(int)Direction.RightLeg].Walls[(int)Direction.LeftLeg], "Wrong usage of SetRefToWalls");
+            if (Sector == Neighbours[(int)Direction.RightLeg].Sector)
+            {
+                Walls[(int)Direction.RightLeg] = Neighbours[(int)Direction.RightLeg].Walls[(int)Direction.RightLeg];
+            }
+            else
+            {
+                Walls[(int)Direction.RightLeg] = Neighbours[(int)Direction.RightLeg].Walls[(int)Direction.LeftLeg];
+            }
+        }
+        if (Neighbours[(int)Direction.TriangleBase] != null)
+        {
+            Debug.Assert(Neighbours[(int)Direction.TriangleBase].Walls[(int)Direction.TriangleBase], "Wrong usage of SetRefToWalls");
+            Walls[(int)Direction.TriangleBase] = Neighbours[(int)Direction.TriangleBase].Walls[(int)Direction.TriangleBase];
+        }
+    }
+
+    public Vector3 GetNeighbourPosition(
+        Direction direction)
+    {
+        int angle = ((int)Rotation.y + (int)direction * 120) % 360;
+        float x = Common.GetAxisCoordinate(wallWidth, angle, Mathf.Sin);
+        float z = Common.GetAxisCoordinate(wallWidth, angle, Mathf.Cos);
+
+        return new Vector3(x + Middle.x, 0.0f, z + Middle.z);
+    }
+
+    public Triangle GetRandomNeighbourAndDeleteWall()
+    {
+        Triangle neighbour = null;
+        HashSet<int> randomNeighbourIndexes = new HashSet<int>();
+        while (randomNeighbourIndexes.Count < Neighbours.Length)
+        {
+            randomNeighbourIndexes.Add(UnityEngine.Random.Range(0, Neighbours.Length));
+        }
+
+        foreach (int i in randomNeighbourIndexes)
+        {
+            if (Neighbours[i] && !Neighbours[i].Visited)
+            {
+                neighbour = Neighbours[i];
+                Debug.Assert(Walls[i]);
+                Destroy(Walls[i]);
+                break;
+            }
+
+        }
+
+        return neighbour;
+    }
+
+    public void Delete()
+    {
+        foreach (GameObject wall in Walls)
+        {
+            DestroyObject(wall, 0.0f);
+        }
+    }
+
+    public GameObject[] Walls
+    {
+        get
+        {
+            return walls;
+        }
+
+        set
+        {
+            walls = value;
+        }
+    }
+
+    public Triangle[] Neighbours
+    {
+        get
+        {
+            return neighbours;
+        }
+
+        set
+        {
+            neighbours = value;
+        }
+    }
+
+    public Vector3 Middle
+    {
+        get
+        {
+            return middle;
+        }
+
+        set
+        {
+            middle = value;
+        }
+    }
+
+    public Vector3 Rotation
+    {
+        get
+        {
+            return rotation;
+        }
+
+        set
+        {
+            rotation = value;
+        }
+    }
+
+    public int Level
+    {
+        get
+        {
+            return level;
+        }
+
+        set
+        {
+            level = value;
+        }
+    }
+
+    public int Sector
+    {
+        get
+        {
+            return sector;
+        }
+
+        set
+        {
+            sector = value;
+        }
+    }
+
+    public Drawing ShouldBeDrawn
+    {
+        get
+        {
+            return shouldBeDrawn;
+        }
+
+        set
+        {
+            shouldBeDrawn = value;
+        }
+    }
+
+    public bool Visited
+    {
+        get
+        {
+            return visited;
+        }
+
+        set
+        {
+            visited = value;
+        }
+    }
+
+    public bool IsDeadEnd
+    {
+        get
+        {
+            return isDeadEnd;
+        }
+
+        set
+        {
+            isDeadEnd = value;
+        }
+    }
+
+    //private:
+    private Vector3 middle;
+    private Vector3 rotation;
+    private GameObject[] walls = new GameObject[3];
+    private Triangle[] neighbours = new Triangle[3];
+    private float wallWidth = 0;
+    private int level;
+    private int sector; // TODO: make enum from this
+    private Drawing shouldBeDrawn;
+    private bool visited;
+    private bool isDeadEnd;
+
+    private void Initialize(GameObject wall, Vector3 middle, Vector3 rotation, int level, int sector, Drawing shouldBeDrawn)
+    {
+        Debug.Assert(level >= 0);
+        Debug.Assert(sector >= 0);
+        wallWidth = wall.transform.lossyScale.z;
+        gameObject.name = "Triangle";
+        Middle = middle;
+        Rotation = rotation;
+        Level = level;
+        Sector = sector;
+        ShouldBeDrawn = shouldBeDrawn;
+        visited = false;
+        IsDeadEnd = false;
+    }
+
+    private GameObject GetLeftLeg(
+        GameObject wall,
+        Vector3 middle,
+        Vector3 rotation)
+    {
+        Vector3 leftLegPos = new Vector3(-wallWidth / 4.0f, 0.0f, (wallWidth * Mathf.Sqrt(3.0f)) / 12.0f);
+        Vector3 leftLegRotation = new Vector3(0.0f, 30.0f, 0.0f);
+
+        GameObject leftLeg = Instantiate(wall, leftLegPos, Quaternion.Euler(leftLegRotation)) as GameObject;
+
+        return leftLeg;
+    }
+
+    private GameObject GetRightLeg(
+        GameObject wall,
+        Vector3 middle,
+        Vector3 rotation)
+    {
+        Vector3 rightLegPos = new Vector3(wallWidth / 4.0f, 0.0f, (wallWidth * Mathf.Sqrt(3.0f)) / 12.0f);
+        Vector3 rightLegRotation = new Vector3(0.0f, 150.0f, 0.0f);
+
+        GameObject rightLeg = Instantiate(wall, rightLegPos, Quaternion.Euler(rightLegRotation)) as GameObject;
+
+        return rightLeg;
+    }
+
+    private GameObject GetTriangleBase(
+        GameObject wall,
+        Vector3 middle,
+        Vector3 rotation)
+    {
+        Vector3 basePos = new Vector3(0.0f, 0.0f, -(wallWidth * Mathf.Sqrt(3.0f) / 6.0f));
+        Vector3 baseRotation = new Vector3(0.0f, 90.0f, 0.0f);
+
+        GameObject triangleBase = Instantiate(wall, basePos, Quaternion.Euler(baseRotation)) as GameObject;
+
+        return triangleBase;
+    }
+
+    private void HoldWalls()
+    {
+        for (int i = 0; i < Walls.Length; i++)
+        {
+            if (Walls[i] != null)
+            {
+                Walls[i].transform.parent = gameObject.transform;
+            }
+        }
+    }
+
+    private void SetTriangleOnRightPosition()
+    {
+        gameObject.transform.position = Middle;
+        gameObject.transform.Rotate(Rotation);
+    }
+}
